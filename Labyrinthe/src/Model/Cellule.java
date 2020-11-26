485d89c69509cf5e21958ff54078846a28fd4fef
STANISLASD1 <74603003+STANISLASD1@users.noreply.github.com>
2020-11-26 17:03:04 +0100

    a

--- Labyrinthe/src/Model/Cellule.java
+++ Labyrinthe/src/Model/Cellule.java
@@ -9,7 +9,7 @@ public class Cellule
     private Occupant occupant;
     private boolean visitee;
     
-<<<<<<< HEAD
+
     public Cellule( Cellule nord, Cellule sud, Cellule est, Cellule ouest,Occupant occupant, boolean visitee )
     {
         this.nord=nord;
@@ -17,7 +17,7 @@ public class Cellule
         this.est=est;
         this.ouest= ouest;
         this.occupant = occupant ; 
-=======
+
     public Cellule()
     {
         nord = null;
@@ -25,22 +25,22 @@ public class Cellule
         est = null;
         ouest = null;
         occupant = null;
->>>>>>> d8aa6e0bfc3eb9030bd2e0edd075a000219dcd1f
+
         visitee = false;
     }
     public  estLibre()
     {
-<<<<<<< HEAD
+
         if(visitee == false)
         {
          return System.out.println("la case est libre");
         }
-=======
+
         if(occupant == null)
             return true;
         else
             return false;
->>>>>>> d8aa6e0bfc3eb9030bd2e0edd075a000219dcd1f
+
     }
    
 
--- Labyrinthe/src/Model/Occupant.java
+++ Labyrinthe/src/Model/Occupant.java
@@ -1,6 +1,6 @@
 package Model;
 
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,24 +16,24 @@ public abstract class Occupant
 {
 
     protected Cellule Occupé ;
-=======
+
 public abstract class Occupant
 {
     protected boolean CelluleOccupé ;
->>>>>>> d8aa6e0bfc3eb9030bd2e0edd075a000219dcd1f
+
  // Constructeur
     public Occupant(Cellule Occupé) 
     {
-<<<<<<< HEAD
+
         this.Occupé = Occupé; 
     }
     
     static void prendreOccupant()
     {
     cellule.prendreOccupant ; 
-=======
+
         CelluleOccupé = false;
->>>>>>> d8aa6e0bfc3eb9030bd2e0edd075a000219dcd1f
+
     }
     // getters 
     
