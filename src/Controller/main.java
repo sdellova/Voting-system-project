a1b586e887f0bf473b4de317226b010371af5831
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-11-27 19:17:00 +0100

    a

--- src/Controller/main.java
+++ src/Controller/main.java
@@ -1,15 +1,16 @@
 package Controller;
 
-import View.Interface_Graph;
-import static Model.Connecting.connect;
+//import View.Interface_Graph;
+import Model.Connecting;
+import static Model.Connecting.getDBConnection;
 import java.sql.Connection;
 
 public class main
 {
     public static void main(String args[])
     {
-        Connection conn = connect();
-        try {
+        Connecting.initDB();
+        /*try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -28,10 +29,10 @@ public class main
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+       /* java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Interface_Graph().setVisible(true);
             }
-        });
+        });*/
     }
 }
--- src/Model/Connecting.java
+++ src/Model/Connecting.java
@@ -2,23 +2,45 @@ package Model;
 
 import java.sql.*;
 
-public class Connecting
-{
-    public static Connection connect()
-    {
-        try
-        {
-        String url="jdbc:mysql://localhost:3306/voting-system-project";
-        String user="root";
-        String pass="";
-        Connection conn = DriverManager.getConnection(url, user, pass);
-        System.out.println("connexion ok");
-        return conn;
-        }
-        catch (Exception ex)
-        {
-            System.out.println(ex.getMessage() + "ERRORRRRRRRRRRRRRRRRRRRRRRR");
+public class Connecting {
+
+    public static Connection getDBConnection(String DBName) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/" + DBName;
+            String user = "root";
+            String pass = "";
+            Connection conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("connexion ok");
+            return conn;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
             return null;
+        }
+    }
+
+    public static void initDB() {
+        String createDB = "CREATE DATABASE IF NOT EXISTS voting";
+        String createTable1 = "CREATE TABLE IF NOT EXISTS candidate"
+                + "(firstname TEXT(30), "
+                + "lastname TEXT(30))";
+        String createTable2 = "CREATE TABLE IF NOT EXISTS user"
+                + "(firstname TEXT(30), "
+                + "lastname TEXT(30))";
+        String createTable3 = "CREATE TABLE IF NOT EXISTS voter"
+                + "(firstname TEXT(30), "
+                + "lastname TEXT(30))";
+        try {
+            Connection dbConnection = getDBConnection("");
+            Statement statement = dbConnection.createStatement();
+            //execute the SQL statement
+            statement.execute(createDB);
+            dbConnection = getDBConnection("voting");
+            statement = dbConnection.createStatement();
+            statement.execute(createTable1);
+            statement.execute(createTable2);
+            statement.execute(createTable3);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
